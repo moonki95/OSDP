b23bb2bf12aede04776c2d3604c28e3fcf9f0042
chhee66 <chhee66@naver.com>
2019-05-14 19:58:25 +0900

    Add files via upload

---
+++ Project/src/user/UserDAO.java
@@ -0,0 +1,46 @@
+package user;
+
+//DAO = '데이터베이스 접근 객체'의 약자. 실질적으로 데이터베이스에서 회원정보를 불러오거나 데이터베이스에 회원 정보를 넣을 때 사용한다. 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	public UserDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/BBS";
+			String dbID = "root";
+			String dbPassword = "9866";
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public int login(String userID, String userPassword) {
+		String SQL = "SELECT userPassword FROM USER WHERE userID =?";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				if (rs.getString(1).equals(userPassword))
+					return 1; // 로그인 성공
+				else
+					return 0; // 비밀번호 불일치
+			}
+			return -1; // 아이디가 없음
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -2; // 데이터베이스 오류
+	}
+
+}
